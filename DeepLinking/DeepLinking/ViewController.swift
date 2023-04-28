8a8897535855c16070f30e52932141b705586afa
Janvi Arora <jjanvi.arora18@gmail.com>
2023-04-28 13:43:37 +0530

    deep linking project creation

---
+++ DeepLinking/DeepLinking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeepLinking
+//
+//  Created by Powerplay on 28/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
